b04fc084b52c2898e0888f85edce9af0d31c6374
minhhung123 <prohungnguyenminh@gmail.com>
2021-04-26 16:30:19 +0700

    Initial commit

---
+++ contracts/Election.sol
@@ -0,0 +1,12 @@
+// SPDX-License-Identifier: MIT
+pragma solidity >=0.4.11;
+
+contract Election {
+  // Constructor
+  string public candidate;
+  constructor () public {
+    candidate = "Hung";
+  }
+  
+  
+}
